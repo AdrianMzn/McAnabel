d5d358c60c21452b1df3d5dfc666ab1a65bcd305
Adrian-uni <79143545+Adrian-uni@users.noreply.github.com>
2022-09-16 01:25:14 +0200

    push raro

--- anabelle-ristorante/src/components/Home.jsx
+++ anabelle-ristorante/src/components/Home.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import pizza1 from '../images/bg_1.png';
 import pizza2 from '../images/bg_2.png';
+import CarouselFadeExample from './Carousel';
 
 import './Home.css'
 
@@ -93,13 +94,13 @@ export class Home extends Component {
               <span className="visually-hidden">Next</span>
     </button>*/}
 
-            <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
+            <a className="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="sr-only">Previous</span>
             </a>
-            <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
+            <a className="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="sr-only">Next</span>
             </a>
 
           </div>
@@ -129,6 +130,8 @@ export class Home extends Component {
           </div>
         </section>
 
+
+        <CarouselFadeExample />
 
       </div>
     )
